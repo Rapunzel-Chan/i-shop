25162889b5ce5fc22588296de3b8c49c911658bc
Rapunzel <osina_kv@mail.ru>
2025-05-21 00:38:07 +0300

    add form for users with success message

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from django.contrib import messages
 
-# Create your views here.
 def home(request):
     return render(request, 'home.html')
 
-def contacts(request):
+def contact_view(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        if name and phone and message:
+            messages.success(request, "Спасибо! Ваше сообщение успешно отправлено.")
+
     return render(request, 'contacts.html')
