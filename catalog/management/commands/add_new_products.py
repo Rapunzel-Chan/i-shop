fe0f5a785543a6f9988b1d5a8fb59242adcf663f
Rapunzel <osina_kv@mail.ru>
2025-06-03 23:17:22 +0300

    add catalog_fixture, screenshots, management_commands add_new_products & load_fixtures

---
+++ catalog/management/commands/add_new_products.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from catalog.models import Product, Category
+
+class Command(BaseCommand):
+    help = 'Add test products to the database'
+
+    def handle(self, *args, **kwargs):
+        category, _ = Category.objects.get_or_create(name='Зелень')
+
+        products = [
+            {'name': 'Укроп', 'price': 60.00, 'category': category},
+            {'name': 'Петрушка', 'price': 55.00, 'category': category},
+            {'name': 'Базилик', 'price': 70.00, 'category': category},
+            {'name': 'Шпинат', 'price': 87.00, 'category': category},
+        ]
+
+        for product_data in products:
+            product, created = Product.objects.get_or_create(**product_data)
+            if created:
+                self.stdout.write(self.style.SUCCESS(f'Successfully added product: {product.name} {product.category}'))
+            else:
+                self.stdout.write(self.style.WARNING(f'Product already exists: {product.name} {product.category}'))
