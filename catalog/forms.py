adc18aa180631d7df942481d3c8384228121d2be
Rapunzel <osina_kv@mail.ru>
2025-06-17 23:13:12 +0300

    add forms.py, change productform and product_form.html to use different h2 and buttons depending on creation or updating product

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,19 +1,25 @@
 from django import forms
+from django.forms import ModelForm
 
 from .models import Product
 
-
-class ProductForm(forms.ModelForm):
+class ProductForm(ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'description', 'photo', 'category', 'price']
-        widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
-            'photo': forms.ClearableFileInput(attrs={'class': 'form-control'}),
-            'category': forms.Select(attrs={'class': 'form-control'}),
-            'price': forms.NumberInput(attrs={'class': 'form-control'}),
-        }
+        fields = "__all__"
+
+
+# class ProductForm(forms.ModelForm):
+#     class Meta:
+#         model = Product
+#         fields = ['name', 'description', 'photo', 'category', 'price']
+#         widgets = {
+#             'name': forms.TextInput(attrs={'class': 'form-control'}),
+#             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+#             'photo': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+#             'category': forms.Select(attrs={'class': 'form-control'}),
+#             'price': forms.NumberInput(attrs={'class': 'form-control'}),
+#         }
 
 #
 # class ContactForm(forms.Form):
