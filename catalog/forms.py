48870614d7836d8216f7806de9329179e6fb0e75
Rapunzel <osina_kv@mail.ru>
2025-06-17 23:55:32 +0300

    add clean_name, clean_description, clean_price to ProductForm for validation

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,10 +1,9 @@
-from xmlrpc.client import Boolean
-
 from django import forms
+from django.core.exceptions import ValidationError
 from django.db.models import BooleanField
 from django.forms import ModelForm
 
-from .models import Product
+from .models import Product, Contact
 
 
 class StyleFormMixin:
@@ -17,26 +16,39 @@ class StyleFormMixin:
                 fild.widget.attrs['class'] = "form-control"
 
 
+FORBIDDEN_WORDS = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
+
 class ProductForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Product
         fields = "__all__"
 
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        for word in FORBIDDEN_WORDS:
+            if word.lower() in name.lower():
+                raise ValidationError("Название содержит запрещённые слова. Не надо так!")
+        return name
 
-# class ProductForm(forms.ModelForm):
-#     class Meta:
-#         model = Product
-#         fields = ['name', 'description', 'photo', 'category', 'price']
-#         widgets = {
-#             'name': forms.TextInput(attrs={'class': 'form-control'}),
-#             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
-#             'photo': forms.ClearableFileInput(attrs={'class': 'form-control'}),
-#             'category': forms.Select(attrs={'class': 'form-control'}),
-#             'price': forms.NumberInput(attrs={'class': 'form-control'}),
-#         }
+    def clean_description(self):
+        description = self.cleaned_data['description']
+        for word in FORBIDDEN_WORDS:
+            if word.lower() in description.lower():
+                raise ValidationError("Описание содержит запрещённые слова. Не надо так!")
+        return description
 
-#
-# class ContactForm(forms.Form):
-#     name = forms.CharField(max_length=100)
-#     phone = forms.CharField(max_length=15)
-#     message = forms.CharField(widget=forms.Textarea)
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price is not None and price < 0:
+            raise ValidationError("Цена не может быть отрицательной.")
+        return price
+
+
+# class ContactForm(ModelForm):
+#     model = Contact
+#     fields = ["name", "phone", "message"]
+#     widgets = {
+# #     name: forms.CharField(max_length=100)
+# #     phone = forms.CharField(max_length=15)
+# #     message = forms.CharField(widget=forms.Textarea)
