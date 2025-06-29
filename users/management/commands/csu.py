a9268c48f474e53632e9e0c9a731ec39e84bbe57
Rapunzel <osina_kv@mail.ru>
2025-06-29 08:50:00 +0300

    relocate  admin_password to env

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -1,12 +1,13 @@
 from django.core.management import BaseCommand
-
+from django.conf import settings
 from users.models import User
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        password = settings.ADMIN_PASSWORD
         user = User.objects.create(email="admin@example.com")
-        user.set_password("123rty")
+        user.set_password(password)
         user.is_active = True
         user.is_staff = True
         user.is_superuser = True
